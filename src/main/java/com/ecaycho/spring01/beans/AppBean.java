f87ee4d3fc51825fe8858d6180b69f86d5d70dfb
Erick CP <SABADO-TARDE@LAB707-12>
2014-08-02 12:54:37 -0500

    Esta es una  aplicación básica de la clase 01
    I added a new dependency (Spring Framework) 5 minutes ago.

--- src/main/java/com/ecaycho/spring01/beans/AppBean.java
+++ src/main/java/com/ecaycho/spring01/beans/AppBean.java
@@ -1,8 +1,23 @@
 package com.ecaycho.spring01.beans;
 
+import com.ecaycho.spring01.model.Musico;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AppBean {
     
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("app.xml");
+        AppBean.valoresConstructor(ctx);
+    }
     
+    public static void valoresConstructor(ApplicationContext ctx) {
+        Musico m = (Musico) ctx.getBean("musico");
+        System.out.println("Nombres completos del musico: " + m.getNombre() + " " + m.getApellido());
+        System.out.println("Numero de canciones: " + m.getCancionesConocidas());
+        
+    }
     
-    
+     
 }
+
