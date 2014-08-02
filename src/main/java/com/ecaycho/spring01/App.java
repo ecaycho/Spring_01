79a0d4c45cee9363682a57672fcc6251b5e4e19b
Erick CP <SABADO-TARDE@LAB707-12>
2014-08-02 11:37:10 -0500

    Esta es una  aplicación básica de la clase 01
    I added a new dependency (Spring Framework) 5 minutes ago.

--- src/main/java/com/ecaycho/spring01/App.java
+++ src/main/java/com/ecaycho/spring01/App.java
@@ -5,6 +5,6 @@ public class App
     public static void main( String[] args )
     {
         double num1  = Math.random();
-        System.out.println( "Hello World!" + " - " + num1  );
+        System.out.println( "Hello World!" + " - " + Math.round(num1)  );
     }
 }
