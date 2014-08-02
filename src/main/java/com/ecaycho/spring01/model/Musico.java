eb46ecddff03065a52c6dd5cf0d83ecfdec22413
Erick CP <SABADO-TARDE@LAB707-12>
2014-08-02 12:32:10 -0500

    Esta es una  aplicación básica de la clase 01
    I added a new dependency (Spring Framework) 5 minutes ago.

--- src/main/java/com/ecaycho/spring01/model/Musico.java
+++ src/main/java/com/ecaycho/spring01/model/Musico.java
@@ -1,7 +1,39 @@
 package com.ecaycho.spring01.model;
 
-public class Musico {
+import java.util.List;
 
+public class Musico extends Persona{
+    
+    private int cancionesConocidas;
+    private Instrumento instrumento;
+    private List<Instrumento> instrumentos;
+
+    public int getCancionesConocidas() {
+        return cancionesConocidas;
+    }
+
+    public void setCancionesConocidas(int cancionesConocidas) {
+        this.cancionesConocidas = cancionesConocidas;
+    }
+
+    public Instrumento getInstrumento() {
+        return instrumento;
+    }
+
+    public void setInstrumento(Instrumento instrumento) {
+        this.instrumento = instrumento;
+    }
+
+    public List<Instrumento> getInstrumentos() {
+        return instrumentos;
+    }
+
+    public void setInstrumentos(List<Instrumento> instrumentos) {
+        this.instrumentos = instrumentos;
+    }
+    
+    
+    
     public Musico(int cancionesConocidas) {
     
     
